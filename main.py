4eb4c6dffb54a85a964873e76174fbaaab4be69f
resumetozero <resumetozero3@gmail.com>
2025-09-29 03:36:42 +0530

    merged

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import streamlit as st
 from scrapping import scrape_page, preprocess_text, split_text
-<<<<<<< HEAD
-
-=======
 from parse import parse_ollama
->>>>>>> e76133e (Attached Ollama LLM Model)
 
 import os
 import sys
@@ -36,15 +32,6 @@ with st.expander("Show scrapped content"):
     st.text_area("Scrapped Content", value=st.session_state['dom'], height=300)
 
 if "dom" in st.session_state:
-<<<<<<< HEAD
-    parsed_content=st.text_area("Describe what do you want to Analyze from the scrapped content?", height=100)
-    if st.button("Analyze"):
-        if parsed_content:
-            st.write(f"Analyzing the content for: {parsed_content}")
-            # Here you can add your analysis logic using LLMs or any other method
-
-            st.success("Analysis complete!")
-=======
     query=st.text_area("Describe what do you want to Analyze from the scrapped content?", height=100)
     if st.button("Analyze"):
         if query:
@@ -55,6 +42,5 @@ if "dom" in st.session_state:
             st.success("Analysis complete!")
             st.write(analysis_results)
             
->>>>>>> e76133e (Attached Ollama LLM Model)
         else:
             st.error("Please enter a valid input for analysis.")
